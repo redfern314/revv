975a329a1e4a17266fc1f25468e68132fb5f5807
skinc <shane.skikne@students.olin.edu>
2013-04-09 16:55:39 -0400

    Finished populate for DB

--- models.js
+++ models.js
@@ -19,18 +19,15 @@ exports.populate = function(req, res){
         for (word in wordfreq){
 
             var currentWordFreq = (wordfreq[word]).split(" ");
-            console.log("Adding: " + currentWordFreq[0])
             var newWord = new Word({word: currentWordFreq[0], freq: parseInt(currentWordFreq[1])})
             newWord.save(function (err) {
                 if (err) {
                     console.log("error", err);
                 }
-                else{
-                    console.log("Added " + currentWordFreq[0]);
-                }
             });
         }   
     });    
+    res.send("Database population complete")
 }
 
 exports.Word = Word;
