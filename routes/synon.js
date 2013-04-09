83087edd9d45679a99b8a165c3b8b1ae3e9091a7
TheoT <theodore.thompson@students.olin.edu>
2013-04-09 16:02:01 -0400

    adding thesaurus

--- routes/synon.js
+++ routes/synon.js
@@ -1,5 +1,7 @@
 var models = require('../models');
 var WordPOS= require('wordpos');
+var rem=require('rem');
+
 var wordpos = new WordPOS();
 
 
@@ -11,6 +13,9 @@ exports.getSyns=function(req,res){
 	wordpos.getPOS(text, function(parts){
 		word=parts.adjectives[0]
 		console.log(word)
+		rem.json('http://words.bighugelabs.com/api/2/6967250016703c7776746000825296ea/'+word+'/json').get(function(err,content,res){
+			console.log(content.adjective.syn)
+		});
 		res.send(parts);
 
 	});
