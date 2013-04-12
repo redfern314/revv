1b4b291d0035e9f7fb8141571853781f2c1d7b3b
skinc <shane.skikne@students.olin.edu>
2013-04-11 22:34:44 -0400

    context menu

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,16 +1,27 @@
 
 
 function contextMenuClicked(){
-    chrome.pageAction.show("Clicked");
-    console.log("Clicked");
+    console.log("Clicked Test");
+
+    chrome.contextMenus.create({type: "normal", title: "Word1", id: "word1", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 1")});
+    chrome.contextMenus.create({type: "normal", title: "Word2", id: "word2", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 2")});
+    chrome.contextMenus.create({type: "normal", title: "Word3", id: "word3", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 3")});
+    chrome.contextMenus.create({type: "separator", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 3")});
+
+
+
+    chrome.contextMenus.create({type: "normal", title: "Definition1", parentId: "word1", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 1")});
+    chrome.contextMenus.create({type: "normal", title: "Definition2", parentId: "word2", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 2")});
+    chrome.contextMenus.create({type: "normal", title: "Definition3", parentId: "word3", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 3")});
 }
 
 
-var opt = {
-        title: 'Add to station',
-        documentUrlPatterns: ['http://*.jamendo.com/*/album/*']
-}
-albumMenu = chrome.contextMenus.create(opt);
-chrome.contextMenus.create({title: 'New station...', parentId: albumMenu, onclick: function(e) { addAlbumStation(e.pageUrl); } });
+// var opt = {
+//         title: 'Add to station',
+//         documentUrlPatterns: ['http://*.jamendo.com/*/album/*']
+// }
+// albumMenu = chrome.contextMenus.create(opt);
+// chrome.contextMenus.create({title: 'New station...', parentId: albumMenu, onclick: function(e) { addAlbumStation(e.pageUrl); } });
+chrome.contextMenus.create({type: "normal", title: "Refresh", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("we can even refresh")});
 
-chrome.contextMenus.create({type: "separator", title: "Prolix", contexts: ["editable"], onclick: contextMenuClicked});
+    chrome.contextMenus.create({type: "normal", title: "Word1", id: "word1", contexts: ["editable"], onclick: contextMenuClicked}, function(){console.log("started 1
