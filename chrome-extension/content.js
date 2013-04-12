a4697b756070a2270e0961066a987b04bf25d4e5
Derek Redfern <redfern.derek@gmail.com>
2013-04-11 23:13:40 -0400

    Working on trading msgs between background and content scripts; also trying to highlight words

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,18 +1,46 @@
+
+
+var current_text = '';
+var current_area = null;
 var showReplacements = function(data) {
         console.log(data);
+        /*chrome.runtime.sendMessage(data, function(response) {
+          console.log(response);
+        });*/
+        words = []
+        for (var i = 0; i < data.words.length; i++) {
+            words.push(data.words[i].old)
+        };
+            
+        $(current_area).highlightTextarea({
+              words: words
+        })
 }
+var getMessageFromBackground = function(request, sender, sendResponse) {
+    console.log(request);
+    if (request.message == "refresh") {
+        sendResponse('Refresh request acknowledged')
+        $.post('http://localhost:5000/synon',{text: current_text},showReplacements);
+    }
+}
+
+chrome.runtime.onMessage.addListener(getMessageFromBackground);
+
 $(
     function(){
         if (window.location.href.indexOf("facebook.com") != -1) {
             $("body").keyup(function(event) {
                 text = $(event.target).context.value
                 console.log(text);
+                console.log($(event.target))
+                current_text = text;
                 $.post('http://localhost:5000/synon',{text: text},showReplacements)
             });
         } else if (window.location.pathname.indexOf("twitter.com") != -1) {
             $("body").keyup(function(event) {
                 text = $(event.target).context.value
                 console.log(text);
+                current_text = text;
                 $.post('http://localhost:5000/synon',{text: text},showReplacements)
             });
         }
