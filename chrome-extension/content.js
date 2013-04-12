f7bec2cbcce993e5b70302efcea02468ea5d4710
Derek Redfern <redfern.derek@gmail.com>
2013-04-12 00:27:17 -0400

    Messaging set up content-side; word replacement enabled

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -2,25 +2,35 @@
 
 var current_text = '';
 var current_area = null;
+var facebook = (window.location.href.indexOf("facebook.com") != -1);
+var twitter = (window.location.href.indexOf("twitter.com") != -1);
 var showReplacements = function(data) {
         console.log(data);
         /*chrome.runtime.sendMessage(data, function(response) {
           console.log(response);
         });*/
-        words = []
-        for (var i = 0; i < data.words.length; i++) {
-            words.push(data.words[i].old)
-        };
-            
-        $(current_area).highlightTextarea({
-              words: words
-        })
+        replaceWord("hello","goodbye");
 }
+
 var getMessageFromBackground = function(request, sender, sendResponse) {
     console.log(request);
-    if (request.message == "refresh") {
+    if (request.message == "refresh") { // request = {message: "refresh"}
         sendResponse('Refresh request acknowledged')
         $.post('http://localhost:5000/synon',{text: current_text},showReplacements);
+    } else if (request.message == "replace") { // request = {message: "replace", oldword: "big", newword: "huge"}
+        replaceWord(request.oldword,request.newword);
+    }
+}
+
+var replaceWord = function(oldword,newword) {
+    if (facebook) {
+        text = $(current_area).context.value;
+        newtext = text.replace(oldword,newword);
+        $(current_area).context.value = newtext;
+    } else if (twitter) {
+        text = $(current_area).context.textContent;
+        newtext = text.replace(oldword,newword);
+        $(current_area).context.textContent = newtext;
     }
 }
 
@@ -28,19 +38,22 @@ chrome.runtime.onMessage.addListener(getMessageFromBackground);
 
 $(
     function(){
-        if (window.location.href.indexOf("facebook.com") != -1) {
+        if (facebook) {
             $("body").keyup(function(event) {
                 text = $(event.target).context.value
                 console.log(text);
-                console.log($(event.target))
+                console.log($(event.target).context)
                 current_text = text;
+                current_area = event.target;
                 $.post('http://localhost:5000/synon',{text: text},showReplacements)
             });
-        } else if (window.location.pathname.indexOf("twitter.com") != -1) {
+        } else if (twitter) {
             $("body").keyup(function(event) {
-                text = $(event.target).context.value
+                text = $(event.target).context.textContent
                 console.log(text);
+                console.log($(event.target).context)
                 current_text = text;
+                current_area = event.target;
                 $.post('http://localhost:5000/synon',{text: text},showReplacements)
             });
         }
